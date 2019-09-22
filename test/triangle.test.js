f972fc712746e14f1f994f58345d361301574e92
reza <shoosh2007@gmail.com>
2019-09-23 03:28:33 +0330

    test size of sides of triangle

--- test/triangle.test.js
+++ test/triangle.test.js
@@ -21,6 +21,14 @@ test('Ensure triangle size sides is not equal or less than zero ', () => {
   }
 });
 
+test('Ensure size of sides of triangle is valid ?', () => {
+  try {
+    let triangle = new Triangle(1, 1, 10);
+  } catch (error) {
+    expect(error.message).toBe('Size of sides is not valid!');
+  }
+});
+
 test('Determine triangle is Equilateral', () => {
     let triangle = new Triangle(3, 3, 3);
     expect(triangle.type).toBe('Equilateral');
